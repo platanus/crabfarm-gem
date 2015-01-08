c5e2c441ed63d87fd131044af628c960052bb7fa
Ignacio Baixas <iobaixas@gmail.com>
2015-01-08 19:00:57 -0300

    fix(demo): fixes demo state

--- demo/app/states/btc_stats.rb
+++ demo/app/states/btc_stats.rb
@@ -3,7 +3,7 @@ class BtcStats < Crabfarm::BaseState
   def crawl
     browser.goto('https://www.btc-e.com/')
     indicators = browser.parse :indicators
-    output.price indicators.btc_price
+    output[:price] = indicators.btc_price
   end
 
 end
