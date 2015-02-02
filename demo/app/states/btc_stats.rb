69c8a344a02f0e51f95aab04ce3195d87eefdc8b
Ignacio Baixas <iobaixas@gmail.com>
2015-02-02 17:50:51 -0300

    feat(demo): adds state spec demo

--- demo/app/states/btc_stats.rb
+++ demo/app/states/btc_stats.rb
@@ -2,6 +2,11 @@ class BtcStats < Crabfarm::BaseState
 
   def crawl
     browser.goto('https://www.btc-e.com/')
+
+    if params[:coin] == 'ltc'
+      browser.search('.pairs li a')[4].click
+    end
+
     indicators = browser.parse :indicators
     output[:price] = indicators.btc_price
   end
