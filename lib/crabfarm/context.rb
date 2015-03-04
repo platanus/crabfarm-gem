b57984c581df582e167585fb7b727f0a551f3dc0
Ignacio Baixas <iobaixas@gmail.com>
2015-03-04 13:14:50 -0300

    refactor(context): improves load/release behavior to better handle errors

--- lib/crabfarm/context.rb
+++ lib/crabfarm/context.rb
@@ -12,11 +12,9 @@ module Crabfarm
     end
 
     def load
-      unless @loaded
-        init_phantom_if_required
-        @pool = DriverBucketPool.new build_driver_factory
-        @loaded = true
-      end
+      init_phantom_if_required
+      init_driver_pool
+      @loaded = true
     end
 
     def run_state(_name, _params={})
@@ -27,26 +25,42 @@ module Crabfarm
     end
 
     def reset
-      load
       @store.reset
-      @pool.reset
+      @pool.reset unless @pool.nil?
     end
 
     def release
-      if @loaded
-        @pool.release
-        @phantom.stop unless @phantom.nil?
-        @loaded = false
-      end
+      release_driver_pool
+      release_phantom
+      @loaded = false
     end
 
   private
 
+    def init_driver_pool
+      @pool = DriverBucketPool.new build_driver_factory if @pool.nil?
+    end
+
+    def release_driver_pool
+      @pool.release unless @pool.nil?
+      @pool = nil
+    end
+
     def init_phantom_if_required
-      if config.phantom_mode_enabled?
-        @phantom = PhantomRunner.new phantom_config
-        @phantom.start
+      if config.phantom_mode_enabled? and @phantom.nil?
+        @phantom = load_and_start_phantom
       end
+    end
+
+    def load_and_start_phantom
+      new_phantom = PhantomRunner.new phantom_config
+      new_phantom.start
+      return new_phantom
+    end
+
+    def release_phantom
+      @phantom.stop unless @phantom.nil?
+      @phantom = nil
     end
 
     def build_driver_factory
