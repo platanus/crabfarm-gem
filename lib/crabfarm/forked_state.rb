5815a5a46199d2d2c87eb0ef6d604e1bb253216a
Ignacio Baixas <iobaixas@gmail.com>
2015-03-12 10:41:44 -0300

    refactor(forked_state): switches to Delegator

--- lib/crabfarm/forked_state.rb
+++ lib/crabfarm/forked_state.rb
@@ -1,13 +1,12 @@
 module Crabfarm
-  class ForkedState
-    extend Forwardable
-
-    def_delegators :@state, :params, :get, :fetch, :event, :alert, :info, :parse, :method_missing, :respond_to?
+  class ForkedState < Delegator
 
     def initialize(_state, _name, _mutex)
       @state = _state
       @name = _name
       @mutex = _mutex
+
+      super @state
     end
 
     def driver
@@ -18,10 +17,22 @@ module Crabfarm
       @browser ||= @state.browser(@name)
     end
 
+    def output
+      raise ScriptError.new 'Use lock_output to access output in forked states'
+    end
+
     def lock_output
       @mutex.synchronize {
         yield @state.output
       }
+    end
+
+    def __getobj__
+      @state
+    end
+
+    def __setobj__(obj)
+      @state = obj
     end
   end
 end
