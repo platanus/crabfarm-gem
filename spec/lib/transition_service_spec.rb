a0f4c96253473d00525fba2d2cbbd9955254d5f1
Ignacio Baixas <iobaixas@gmail.com>
2015-04-17 09:51:53 -0300

    feat(transition_service): adds decorator support for navigators

--- spec/lib/transition_service_spec.rb
+++ spec/lib/transition_service_spec.rb
@@ -34,4 +34,31 @@ describe Crabfarm::TransitionService do
     end
   end
 
+  describe "self.with_navigator_decorator" do
+    it "should apply decorator on every generated navigator" do
+      decorator = double('decorator')
+      expect(decorator).to receive(:decorate).with(kind_of mock_class) { mock_class.new nil, nil }
+
+      Crabfarm::TransitionService.with_navigator_decorator(decorator) do
+        state.transition mock_class
+      end
+    end
+
+    it "should apply chained decorators on generated navigators, from inner to outer scope" do
+      mock_class_b = Class.new(Crabfarm::BaseNavigator) { def run; end }
+
+      decorator_a = double('decorator_a')
+      decorator_b = double('decorator_b')
+
+      expect(decorator_a).to receive(:decorate).with(kind_of mock_class) { mock_class_b.new nil, nil }
+      expect(decorator_b).to receive(:decorate).with(kind_of mock_class_b) { mock_class.new nil, nil }
+
+      Crabfarm::TransitionService.with_navigator_decorator(decorator_b) do
+        Crabfarm::TransitionService.with_navigator_decorator(decorator_a) do
+          state.transition mock_class
+        end
+      end
+    end
+  end
+
 end
