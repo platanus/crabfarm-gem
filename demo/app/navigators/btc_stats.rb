40b559bdca3ea9e52290651b18003e0c8764c87b
Ignacio Baixas <iobaixas@gmail.com>
2015-05-13 14:43:42 -0300

    feat(demo): adds live configuration examples

--- demo/app/navigators/btc_stats.rb
+++ demo/app/navigators/btc_stats.rb
@@ -1,5 +1,10 @@
 class BtcStats < Crabfarm::BaseNavigator
 
+  live do
+    use_memento 'btce-2'
+    use_params coin: 'ltc'
+  end
+
   def run
     browser.goto('https://www.btc-e.com/')
 
--- demo/app/reducers/indicators_reducer.rb
+++ demo/app/reducers/indicators_reducer.rb
@@ -2,6 +2,8 @@ class IndicatorsReducer < Crabfarm::BaseReducer
 
   has_float :price, greater_than: 0.0
 
+  live delegate: BtcStats
+
   def run
     self.price = at_css('.orderStats')
   end
