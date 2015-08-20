5b01f3d78576fde95988c8b129de784f4a9fa7ac
Ignacio Baixas <iobaixas@gmail.com>
2015-08-20 16:23:09 -0300

    feat(rspec_runner): changes run_spec_for to runs_single_spec_for.
    The new method ensures only one example is ran, others are skipped.

--- lib/crabfarm/utils/rspec_runner.rb
+++ lib/crabfarm/utils/rspec_runner.rb
@@ -6,31 +6,23 @@ module Crabfarm
       extend self
 
       class SilentFormatter
-        ::RSpec::Core::Formatters.register self
-
         def initialize(_output)
+          # do nothing with output (very silent!)
         end
       end
 
-      class DummyOptions
-        def configure(_); end
-      end
-
-      def run_spec_for(_target, _filter=nil)
-
-        _target = Array(_target)
-        # TODO: check target exist?
-
-        first_time_config
+      def run_single_spec_for(_target, _tag=nil)
+        setup_rspec_once
 
         begin
-          ::RSpec.configuration.files_or_directories_to_run = _target
-          ::RSpec.configuration.inclusion_filter = _filter if _filter
+          configuration.reset
 
-          runner = ::RSpec::Core::Runner.new DummyOptions.new
-          runner.run $stderr, $stdout
+          runner = ::RSpec::Core::Runner.new config_options(_target)
+          runner.setup $stderr, $stdout
+          example = skip_all_but_one _tag
+          runner.run_specs(world.ordered_example_groups)
 
-          ::RSpec.world.example_groups.map { |g| g.filtered_examples }.flatten
+          example
         ensure
           ::RSpec.clear_examples
         end
@@ -38,15 +30,53 @@ module Crabfarm
 
     private
 
-      def first_time_config
-        unless @ready
-          options = ::RSpec::Core::ConfigurationOptions.new [
-            '-f','Crabfarm::Utils::RSpecRunner::SilentFormatter'
-          ]
+      def world
+        ::RSpec.world
+      end
 
-          options.configure ::RSpec.configuration
+      def configuration
+        ::RSpec.configuration
+      end
+
+      def setup_rspec_once
+        unless @ready
+          ::RSpec::Core::Formatters.register SilentFormatter
           @ready = true
         end
+      end
+
+      def config_options(_target)
+        ::RSpec::Core::ConfigurationOptions.new [
+            _target,
+            '-f','Crabfarm::Utils::RSpecRunner::SilentFormatter'
+        ]
+      end
+
+      def skip_all_but_one(_tag)
+        best_example = world.all_examples.inject(nil) do |best, example|
+          example.metadata[:skip] = true
+          if is_better_example? example, best, _tag
+            example
+          else
+            best
+          end
+        end
+
+        best_example.metadata[:skip] = false if best_example
+        best_example
+      end
+
+      def is_better_example?(_new, _old, _tag)
+        return true if _old.nil?
+
+        new_tagged = !!(_new.metadata[_tag])
+        old_tagged = !!(_old.metadata[_tag])
+
+        # preffer tagged
+        return new_tagged if new_tagged != old_tagged
+
+        # preffer higher line number
+        return _new.metadata[:line_number] > _old.metadata[:line_number]
       end
 
     end
