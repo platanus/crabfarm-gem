ab5e5cf0897640054f68a5ae7fa4f4ae1b92d59b
Ignacio Baixas <iobaixas@gmail.com>
2015-09-24 17:42:03 -0300

    feat(utils.processes): adds stream dumper to manage streams in a more performant way

--- lib/crabfarm/utils/processes.rb
+++ lib/crabfarm/utils/processes.rb
@@ -1,30 +1,33 @@
 require 'childprocess'
+require 'crabfarm/utils/stream_dumper'
 
 ChildProcess.posix_spawn = true
 
-module Crabfarm
-  module Utils
-    module Processes
+module Crabfarm::Utils
 
-      def self.start_logged_process(_name, _cmd, _logger, _env={})
-        ro, wo = IO.pipe
-        re, we = IO.pipe
+  module Processes
+    def self.start_logged_process(_name, _cmd, _logger, _env={})
 
-        proc = ChildProcess.build(*_cmd)
-        proc.environment.merge! _env
-        proc.io.stdout = wo
-        proc.io.stderr = we
-        proc.start
+      ro, wo = IO.pipe
+      re, we = IO.pipe
 
-        wo.close
-        we.close
+      proc = ChildProcess.build(*_cmd)
+      proc.environment.merge! _env
+      proc.io.stdout = wo
+      proc.io.stderr = we
+      proc.start
 
-        Thread.new { ro.each_line { |l| _logger.info "[#{_name.upcase}] #{l}" } }
-        Thread.new { re.each_line { |l| _logger.warn "[#{_name.upcase}] #{l}" } }
+      # close write endpoints after fork
+      wo.close
+      we.close
 
-        proc
-      end
+      # register log consumers
+      StreamDumper.register_stream(ro) { |l| _logger.info "[#{_name.upcase}] #{l}" }
+      StreamDumper.register_stream(re) { |l| _logger.warn "[#{_name.upcase}] #{l}" }
 
+      proc
     end
   end
+
 end
+
